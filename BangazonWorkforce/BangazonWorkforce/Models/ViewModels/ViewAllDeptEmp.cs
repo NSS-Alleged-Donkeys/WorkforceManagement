c4cef2f05bfe16bf52fed9d7e1f862ecef925d3c
Madison <madisonpeper@gmail.com>
2018-11-15 11:32:07 -0600

    going to checkout DT3 PR

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/ViewAllDeptEmp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class ViewAllDeptEmp
+    {
+        public List<Department> AllDepartments { get; set; }
+        public List<Employee> AllEmployees { get; set; }
+    }
+}
