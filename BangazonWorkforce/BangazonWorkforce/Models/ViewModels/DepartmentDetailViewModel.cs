63a93326065b363f8136c7a0104700e4f2729f9b
davidnicholastaylor <dntaylor14@gmail.com>
2018-11-15 15:12:45 -0600

    committing to restart my computer

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/DepartmentDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class DepartmentDetailViewModel
+    {
+        public IEnumerable<Employee> Employees;
+    }
+}
