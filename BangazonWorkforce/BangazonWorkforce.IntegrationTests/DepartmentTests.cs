ea2225df08fbd6fbda85efdd5da4e3c59eb1a8cb
davidnicholastaylor <dntaylor14@gmail.com>
2018-11-16 14:49:39 -0600

    test works for displaying department name

--- BangazonWorkforce/BangazonWorkforce.IntegrationTests/DepartmentTests.cs
+++ BangazonWorkforce/BangazonWorkforce.IntegrationTests/DepartmentTests.cs
@@ -1,11 +1,18 @@
 using AngleSharp.Dom.Html;
 using BangazonWorkforce.IntegrationTests.Helpers;
+using BangazonWorkforce.Models.ViewModels;
+using BangazonWorkforce.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
+using AngleSharp.Dom;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace BangazonWorkforce.IntegrationTests
 {
@@ -56,6 +63,35 @@ namespace BangazonWorkforce.IntegrationTests
                 response.Content.Headers.ContentType.ToString());
         }
 
+
+
+        [Fact]
+        public async Task Get_DetailsDisplayEmployees() {
+
+            // Arrange
+            
+            Department department = (await GetAllDepartments()).First();
+
+            int id = department.Id;
+            string url = $"/department/details/{id}";
+            string deptName = department.Name;
+            
+            
+            // Act
+
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+            // Assert
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
+            IHtmlDocument detailPage = await HtmlHelpers.GetDocumentAsync(response);
+            IHtmlCollection<IElement> viewData = detailPage.QuerySelectorAll("h2");
+            Assert.Contains(viewData, h2 => h2.TextContent.Trim() == deptName);
+        }
+
         [Fact]
         public async Task Post_CreateAddsDepartment()
         {
@@ -88,6 +124,29 @@ namespace BangazonWorkforce.IntegrationTests
             Assert.Contains(
                 indexPage.QuerySelectorAll("td"), 
                 td => td.TextContent.Contains(newDepartmentBudget));
+        }
+
+        private async Task<List<Employee>> GetAllEmployees()
+        {
+            using (IDbConnection conn = new SqlConnection(Config.ConnectionSring))
+            {
+                IEnumerable<Employee> allEmployees =
+                    await conn.QueryAsync<Employee>(@"SELECT Id, FirstName, LastName, 
+                                                              IsSupervisor, DepartmentId 
+                                                         FROM Employee
+                                                     ORDER BY Id");
+                return allEmployees.ToList();
+            }
+        }
+
+        private async Task<List<Department>> GetAllDepartments()
+        {
+            using (IDbConnection conn = new SqlConnection(Config.ConnectionSring))
+            {
+                IEnumerable<Department> allDepartments =
+                    await conn.QueryAsync<Department>(@"SELECT Id, Name, Budget FROM Department");
+                return allDepartments.ToList();
+            }
         }
     }
 }
